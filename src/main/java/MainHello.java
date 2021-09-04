12211613e0c1072ca661c27bc660559615539454
Karina Martinez Guerrero <karinamguerrero@estudiantec.cr>
2021-09-03 22:53:49 -0600

    Eliminar archivos innecesarios

--- src/main/java/MainHello.java
+++
@@ -1,7 +0,0 @@
-import java.io.IOException;
-
-public class MainHello {
-    public static void main(String[] args) throws IOException, IOException {
-        System.out.println("Hello world");
-    }
-}
