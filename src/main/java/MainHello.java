1a0dd5645fed3d1c2ef1f6801da55b844faea33a
Karina Martinez Guerrero <karinamguerrero@estudiantec.cr>
2021-09-03 22:42:45 -0600

    Tabla incompleta

---
+++ src/main/java/MainHello.java
@@ -0,0 +1,7 @@
+import java.io.IOException;
+
+public class MainHello {
+    public static void main(String[] args) throws IOException, IOException {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/main/java/UI/Main.java
@@ -0,0 +1,97 @@
+package UI;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import HTTP.ApiResponse;
+import HTTP.Character;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main extends Application {
+
+    private TableView table = new TableView();
+    private static ObjectMapper mapper = new ObjectMapper();
+    private ObservableList<Character> data = FXCollections.observableArrayList();
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException, IOException{
+        Scene scene = new Scene(new Group());
+        stage.setTitle("Table View Sample");
+        stage.setWidth(300);
+        stage.setHeight(500);
+
+        final Label label = new Label("Address Book");
+        label.setFont(new Font("Arial", 20));
+
+        table.setEditable(true);
+
+        TableColumn firstNameCol = new TableColumn("First Name");
+        TableColumn lastNameCol = new TableColumn("Last Name");
+        TableColumn emailCol = new TableColumn("Email");
+
+        table.getColumns().addAll(firstNameCol, lastNameCol, emailCol);
+
+
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        try {
+            HttpGet request = new HttpGet("https://swapi.dev/api/people");
+            CloseableHttpResponse response = httpClient.execute(request);
+            try {
+                System.out.println(response.getProtocolVersion());              // HTTP/1.1
+                System.out.println(response.getStatusLine().getStatusCode());   // 200
+                System.out.println(response.getStatusLine().getReasonPhrase()); // OK
+                System.out.println(response.getStatusLine().toString());        // HTTP/1.1 200 OK
+
+                HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    var result = EntityUtils.toString(entity);
+                    //List<Character> parsedResponse = mapper.readValue(result, new TypeReference<ArrayList<Character>>(){});
+                    System.out.println(result);
+                    //data = (ObservableList<Character>) parsedResponse;
+                }
+            } finally {
+                response.close();
+            }
+        } finally {
+            httpClient.close();
+        }
+        table.setItems(data);
+
+        final VBox vbox = new VBox();
+        vbox.setSpacing(5);
+        vbox.setPadding(new Insets(10, 0, 0, 10));
+        vbox.getChildren().addAll(label, table);
+
+        ((Group) scene.getRoot()).getChildren().addAll(vbox);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
